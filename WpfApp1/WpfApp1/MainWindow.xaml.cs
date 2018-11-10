00c5d75f177820805889841b6d9e3239ac9b46d8
peritrigkas <30832294+peritrigkas@users.noreply.github.com>
2018-11-10 00:34:10 +0000

    Initial Commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Empolyee e1 = new Empolyee();
+            e1.Name = " Peri ";
+
+            Empolyee e2 = new Empolyee();
+            e2.Name = " Trigkas ";
+            Output.Text = e1.Name + " " + e2.Name;
+        }
+    }
+}
