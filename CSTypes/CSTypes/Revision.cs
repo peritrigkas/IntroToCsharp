1ef071a2a3859a5c888e4c67a6462a3420dc319e
peritrigkas <30832294+peritrigkas@users.noreply.github.com>
2018-11-13 22:51:10 +0000

    Passing values using REF and OUT

---
+++ CSTypes/CSTypes/Revision.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSTypes
+{
+    public class Revision
+    {
+        public string Subject { get; set; }
+        public int Hours { get; set; } 
+        
+    }
+    [TestClass]
+
+    public class Study : Revision
+    {
+        [TestMethod]
+        public void Work()
+        {
+            Revision revision = new Revision();
+            revision.Hours = 4;
+            revision.Subject = "Algebra";
+            int Hours;
+            HomeWork(out revision, out Hours);
+
+            Assert.IsTrue(revision.Subject == "History");
+            Assert.IsTrue(Hours == 7);
+        }
+        void HomeWork( out Revision revision, out int Hours)
+        {
+            revision = new Revision();
+            revision.Subject = "History";
+            Hours = 7;
+            
+
+        }
+
+        
+
+
+
+    }
+        
+    
+}
